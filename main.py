1497cadfa658a58346eb61e70d67f799c695a3e8
Gurpreet Singh <68956821+Guru-21@users.noreply.github.com>
2021-03-27 13:26:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+import smtplib
+
+otp_num = random.randint(1000,10000)   #genrate a 4-digit otp number
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()  # for security...
+server.login('mithaimadhuri3421@gmail.com',password='kkumptfmirwsbwlw')  #login with 2 step verification
+msg = 'Hello user,Your OTP is '+str(otp_num)
+server.sendmail('mithaimadhuri3421@gmail.com','technoguru21.10@gmail.com',msg)
+server.quit()
+
+#verified OTP
+user_otp = int(input('Enter your recived OTP :'))
+
+if(user_otp == otp_num):
+    print('Your OTP Access Successfully! :)')
+else:
+    print('Your given OTP is wrong, please try again. :(')
